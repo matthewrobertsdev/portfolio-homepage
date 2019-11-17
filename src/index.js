89879e7b34b609156f98f6a2e85299bf577c6bf0
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-11-16 22:41:28 -0500

    renamed page and url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 //document.body.style.backgroundColor = "rgb(0, 166, 255)";
-document.title='Matt Roberts | Software Developer';
+document.title='Matt Roberts | Developer';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
