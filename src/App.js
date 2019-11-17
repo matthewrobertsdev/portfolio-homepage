2de5132af68423a514e3e199e30d62602cb107e2
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-11-16 22:28:40 -0500

    Shortened main header

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import ProjectView from './ProjectView';
 function App() {
   return (
     <div>
-      <h1 class="title text-color">
-        My name is Matthew Roberts and I am a software developer and tutor. This page has links to some projects I have
-        on GitHub, and tells about them some too.
-    </h1>
+      <h1 class="title text-color"> Matthew Roberts </h1>
+      <h1 class="title text-color"> Software Developer and Tutor </h1>
     <a class="link-button grid-button" href="https://github.com/matthewrobertsdev">My Github</a>
     <br></br><br></br>
     {createProjectViews()}
