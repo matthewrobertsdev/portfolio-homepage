e184c9dad40584f3d5aa3dac768a230ef829bb07
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2019-11-18 15:09:03 -0500

    Start on adding images to page

--- src/ProjectInfos.js
+++ src/ProjectInfos.js
@@ -3,11 +3,13 @@ import LinkModel from './LinkModel';
 class ProjectInfo{
     title='';
     description='';
+    media=[];
     links=[];
 
-    constructor(title, description, links){
+    constructor(title, description, media,links){
         this.title=title;
         this.description=description;
+        this.media=media;
         this.links=links;
     }
 }
@@ -15,33 +17,34 @@ class ProjectInfo{
 export default ProjectInfo;
 
 export const keyboard=new ProjectInfo('keyboard', 'A Java Application that Let\'s '+
-'Your Computer Keyboard be a Piano Keyboard', 
+'Your Computer Keyboard be a Piano Keyboard', ['./keyboard_view.png'],
 [new LinkModel('Website for keyboard', 'https://matthewrobertsdev.github.io/keyboard/'),
 new LinkModel('keyboard Github Repo', 'https://github.com/matthewrobertsdev/keyboard')])
 
 export const bookmarkPage=new ProjectInfo('Bookmark Page', 'A react and redux application '+
 'that let\'s you add, edit, delete, rearrange, save, and load bookmarks, with dark mode and '+
-'light mode', [new LinkModel('Bookmark Page', 'https://matthewrobertsdev.github.io/BookmarkPage/index'),
+'light mode', [], [new LinkModel('Bookmark Page', 'https://matthewrobertsdev.github.io/BookmarkPage/index'),
 new LinkModel('Bookmark Page Github Repo', 'https://github.com/matthewrobertsdev/bookmark-page')])
 
 export const contactQR=new ProjectInfo('ContactQR', 'A project for an iOS app that lets you '+
-'share your contact info using QR codes', [new LinkModel('ContactQR Github repo', 
+'share your contact info using QR codes', [],
+[new LinkModel('ContactQR Github repo', 
 'https://github.com/matthewrobertsdev/ContactQR')])
 
 export const dbConnectionManager=new ProjectInfo('DBConnectionManager for SQL in Java', 
-'A Class for Managing Connections from Java Code to a SQL Database', 
+'A Class for Managing Connections from Java Code to a SQL Database', [],
 [new LinkModel('DBConnectionManager Github Repo', 
 'https://github.com/matthewrobertsdev/DBConnectionManager-SQL-Java')])
 
 export const softwareDeveloperLinks=new ProjectInfo('Software Developer Link Page', 
-'A Page of Some Links for People Learning Software Development Built with React', 
+'A Page of Some Links for People Learning Software Development Built with React', [],
 [new LinkModel('Developer Links Page', 
 'https://matthewrobertsdev.github.io/SoftwareDevelopmentLinks/index'),
 new LinkModel('Developer Links Github Repo', 
 'https://github.com/matthewrobertsdev/software-development-links')])
 
 export const portfolioPage=new ProjectInfo('Portfolio Page Source', 
-'The Source to My Portfolio Page', 
+'The Source to My Portfolio Page', [],
 [new LinkModel('Portfolio Page Source', 
 'https://github.com/matthewrobertsdev/portfolio/tree/source')])
 
--- src/ProjectView.js
+++ src/ProjectView.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import './App.css';
+import kv from './keyboard_view.png'
 
 function ProjectView(props) {
     return (
       <div>
-        <h2 class="text-color"> {props.projectInfo.title}</h2>
-        <h3 class="text-color"> {props.projectInfo.description}</h3>
+        <h2 className="text-color"> {props.projectInfo.title}</h2>
+        <h3 className="text-color"> {props.projectInfo.description}</h3>
+        {createMedia(props)}
+        <br></br>
         {createLinkButtons(props)}
       </div>
     );
@@ -13,8 +16,15 @@ function ProjectView(props) {
     
   }
 
+  function createMedia(props){
+    const images = require.context('./', true);
+    return props.projectInfo.media.map((media)=>{
+      let img = images(media);
+      return <img src={img} className='image'/>})
+  }
+
   function createLinkButtons(props){
-    return props.projectInfo.links.map((link)=>{return <a class="link-button grid-button" 
+    return props.projectInfo.links.map((link)=>{return <a className="link-button grid-button" 
   href={link.URL}>{link.name}</a>})
   }
 export default ProjectView
